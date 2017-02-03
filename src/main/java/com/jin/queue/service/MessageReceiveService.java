70b9079605286743da30ca030136622a8e2fc79a
Jin Zuo <jin.zuo@staples.com>
2017-02-03 00:44:18 -0500

    Remove the deprecated interface

--- src/main/java/com/jin/queue/service/MessageReceiveService.java
+++ src/main/java/com/jin/queue/service/MessageReceiveService.java
@@ -1,9 +1,7 @@
 package com.jin.queue.service;
 
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 
-import com.jin.business.consumer.JinConsumer;
 import com.rabbitmq.client.Channel;
 
 public interface MessageReceiveService {
